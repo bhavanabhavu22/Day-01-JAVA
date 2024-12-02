afee67542df4b6f4b299deddd3ac6bf08ecff65c
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-02 14:48:07 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,12 @@
+public class datatype
+{
+    public static void main(String args[])
+    {
+        int a=12;
+        char b='a';
+        float c=3.14f;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
