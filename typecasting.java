10bfc168e1f44350eb3c3b0d4b1c4403b1ea7321
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-02 15:03:12 +0530

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,13 @@
+public class typecasting
+{
+    public static void main(String args[])
+    {
+    //convesion of acii value to char
+    int a=50;
+    char b=(char)(a);
+    char c='7';
+    System.out.println(b);
+    System.out.println(c);
+
+    }
+}
